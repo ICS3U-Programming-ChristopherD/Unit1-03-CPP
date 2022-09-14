4f654a4c502a090e87c7f6b409f2e34a1b77ecde
Krostofer <christopher.di-bert@stu.ocsb.ca>
2022-09-14 22:00:19 +0000

    This program displays Hello, World! with style.

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Chris Di Bert All rights reserved.
+// .
+// Created by: Chris Di Bert
+// Date: Sep.14, 2022
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+    std::cout << "C++ is efficient." << std::endl;
+}
